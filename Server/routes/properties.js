1470ef8a1d80a1b24e728a51f223a5dfef4495fe
khaled-951 <khaled.fajraoui@esprit.tn>
2021-07-26 02:16:59 +0100

    propertySearch_finished

--- Server/routes/properties.js
+++ Server/routes/properties.js
@@ -4,9 +4,60 @@ const mongoose = require('mongoose');
 const propertySchema = require('../models/property');
 const Property = mongoose.model('properties', propertySchema);
 
-router.get('/', async (req, res) => { 
-    console.log(await Property.find( { "gouvernorat" : "TUNIS" } ) );
-    return res.status(200).send("hi");
+router.get('/getMostViewed', async (req, res) => {
+    return res.status(200).send(await Property.find().sort({ views: -1}).limit(6));
+});
+
+router.get('/getPropertyDetailsAndRecommended/:propId', async (req, res) => {
+    return res.status(200).send( { propertyDetails: await Property.findById(req.params.propId),
+        recommended: await Property.find().sort({ views: -1}).limit(6) } );
+});
+
+router.get('/searchQuery/:page', async (req, res) => {
+    let findQuery = {} ;
+
+    if(req.body.searchQuery)
+        findQuery = { ...findQuery, $or: [ {link: { $regex: req.body.searchQuery }}, 
+            {delegation: { $regex: req.body.searchQuery }}, {adresse: { $regex: req.body.searchQuery }},
+            {codeP: { $regex: req.body.searchQuery }}, {agence: { $regex: req.body.searchQuery }}, {description: { $regex: req.body.searchQuery }} ] } ;
+
+    /*if(Number(req.body.priceMin) > 0 && Number(req.body.priceMin) < 999_999_999)
+    {
+        if( (Number(req.body.priceMin) / 1_000 ) >= 1 && (Number(req.body.priceMin) / 1_000_000 ) < 1 )
+        {
+            findQuery = { ...findQuery, price: { $lte: parseInt(Number(req.body.priceMin) / 1000) + "�000" } } ;}
+        else if( (Number(req.body.priceMin) / 1_000_000 ) >= 1 && (Number(req.body.priceMin) / 1_000_000_000 ) < 1 )
+            findQuery = { ...findQuery, price: { $gte: "90�000�000 DT"} } ;
+    }*/
+    
+    if(req.body.bedrooms === "studio")
+        findQuery = { ...findQuery, nbpiece: { $exists: true, $regex: '0' }} ;
+    else if(Number(req.body.bedrooms) > 0 && Number(req.body.bedrooms) < 6 )
+        findQuery = { ...findQuery, nbpiece: { $exists: true, $regex: req.body.bedrooms[0] }} ;
+
+    if(req.body.airConditioned === true )
+        findQuery = { ...findQuery, climatisation: { $exists: true, $ne: "" }} ;
+    else if (req.body.airConditioned === false )
+        findQuery = { ...findQuery, climatisation: { $exists: true, $eq: "" }} ;
+
+    if(req.body.heated === true )
+        findQuery = { ...findQuery, chauffage: { $exists: true, $ne: "" }} ;
+    else if (req.body.heated === false )
+        findQuery = { ...findQuery, chauffage: { $exists: true, $eq: "" }} ; 
+
+    if(req.body.hasSportsEquipment === true )
+        findQuery = { ...findQuery, installations_sportives: { $exists: true, $ne: "" }} ;
+    else if (req.body.hasSportsEquipment === false )
+        findQuery = { ...findQuery, installations_sportives: { $exists: true, $eq: "" }} ;
+        
+    if( req.body.states instanceof Array )
+        findQuery = { ...findQuery, gouvernorat: { $in: req.body.states }  } ;
+
+    if( req.body.typeImm instanceof Array )
+        findQuery = { ...findQuery, typeImm: { $in: req.body.typeImm }  } ;
+
+    console.log(findQuery);
+    return res.status(200).send(await Property.find(findQuery).skip(6 * Number(req.params.page) ).limit(6) );
 });
 
 module.exports = router ;
