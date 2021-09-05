38e29e119a17489de80b2442300fd79cdac53e53
khaled-951 <khaled.fajraoui@esprit.tn>
2021-09-05 18:03:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -35,4 +35,4 @@ app.use(function(err, req, res, next) {
   });
 
 mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser: true, useUnifiedTopology: true}).catch(() => console.log('could not connect to DB'));
-app.listen(process.env.port);
+app.listen(443);
